c9b1bbbaaad75c79b0caf5746e514b0705c7948e
ANI1KET <aniketrouniyar12@gmail.com>
2024-02-01 18:02:07 +0530

    Check_If_The_String_Is_A_Palindrome

---
+++ Check_If_The_String_Is_A_Palindrome.cc
@@ -0,0 +1,121 @@
+// #include <bits/stdc++.h>
+
+// char toLowerCase(char ch)
+// {
+//     if (ch >= 'a' && ch <= 'z')
+//         return ch;
+//     else
+//     {
+//         char temp = ch - 'A' + 'a';
+//         return temp;
+//     }
+// }
+
+// string extractDigitAlpha(string s, int n)
+// {
+//     string a;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (isdigit(s[i]) || isalpha(s[i]))
+//         {
+//             a.push_back(s[i]);
+//         }
+//     }
+
+//     return a;
+// }
+
+// bool checkPalindrome(string s)
+// {
+//     int l = 0;
+//     int r = s.length() - 1;
+
+//     s = extractDigitAlpha(s, r + 1);
+//     r = s.length() - 1;
+
+//     while (l <= r)
+//     {
+//         if (toLowerCase(s[l]) != toLowerCase(s[r]))
+//         {
+//             return false;
+//         }
+//         else
+//         {
+//             l++;
+//             r--;
+//         }
+//     }
+
+//     return true;
+// }
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+
+using namespace std;
+
+char toLowerCase(char ch)
+{
+    if (ch >= 'a' && ch <= 'z')
+        return ch;
+    else
+    {
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
+
+string extractDigitAlpha(string s, int n)
+{
+    string a;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (isdigit(s[i]) || isalpha(s[i]))
+        {
+            a.push_back(toLowerCase(s[i]));
+        }
+    }
+
+    return a;
+}
+
+bool checkPalindrome(string s)
+{
+    int l = 0;
+    int r = s.length() - 1;
+
+    s = extractDigitAlpha(s, r + 1);
+    r = s.length() - 1;
+
+    while (l <= r)
+    {
+        if (s[l] != s[r])
+        // if (toLowerCase(s[l]) != toLowerCase(s[r]))
+        {
+            return false;
+        }
+        else
+        {
+            l++;
+            r--;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    // string s = {"@N !am a#n"};
+    string s = {"A man, a plan, a canal: Panama"};
+    cout << endl
+         << checkPalindrome(s) << endl;
+
+    return 0;
+}
