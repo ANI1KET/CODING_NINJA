06a5d922ff3a0e62123c3a52355d58b9e7285518
ANI1KET <aniketrouniyar12@gmail.com>
2024-02-07 20:40:51 +0530

    Find_Area_of_Rectangle

---
+++ Find_Area_of_Rectangle.cc
@@ -0,0 +1,24 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int l, b;
+//     cin >> l >> b;
+//     cout << l*b;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int length, breadth;
+
+//     cin >> length;
+//     cin >> breadth;
+
+//     cout << length * breadth;
+
+//     return 0;
+// }
