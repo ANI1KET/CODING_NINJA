21ced8cd28a109966d39b4413ce9c252f8bb7f8f
ANI1KET <aniketrouniyar12@gmail.com>
2024-02-16 12:01:27 +0530

    Inplace_rotate_matrix_90_degree

---
+++ Inplace_rotate_matrix_90_degree.cc
@@ -0,0 +1,17 @@
+// #include <bits/stdc++.h>
+// void inplaceRotate(vector<vector<int>> &inputArray)
+// {
+//     int n = inputArray.size();
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         reverse(inputArray[i].begin(), inputArray[i].end());
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j <= i; j++)
+//         {
+//             swap(inputArray[i][j], inputArray[j][i]);
+//         }
+//     }
+// }
