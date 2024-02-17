f8c173e75db24ac88a84207febe76037afaef2af
ANI1KET <aniketrouniyar12@gmail.com>
2024-02-18 02:43:21 +0530

    Left_Rotate_an_Array_by_One

---
+++ Left_Rotate_an_Array_by_One.cc
@@ -0,0 +1,11 @@
+// #include <bits/stdc++.h>
+// vector<int> rotateArray(vector<int>& arr, int n) {
+//     vector<int> a(n);
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         a[(i + (n - 1)) % n] = arr[i];
+//     }
+
+//     return a;
+// }
