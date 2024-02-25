51efe181303348a56bcc21841ef9fd74d7ffb457
ANI1KET <aniketrouniyar12@gmail.com>
2024-02-25 14:59:46 +0530

    Rearrange_Array

---
+++ Rearrange_Array.cc
@@ -0,0 +1,15 @@
+// #include <bits/stdc++.h>
+// void rearrangeArray(int arr[], int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         int a = arr[i];
+//         int b = arr[a];
+//         arr[i] = a + ((b % n) * n);
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         arr[i] = arr[i] / n;
+//     }
+// }
