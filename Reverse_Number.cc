55c69aaae4fa5b2dec462ece5ed48e1a62078209
ANI1KET <aniketrouniyar12@gmail.com>
2024-02-26 14:41:02 +0530

    Reverse_Number

---
+++ Reverse_Number.cc
@@ -0,0 +1,12 @@
+// #include <bits/stdc++.h>
+// long long int reverseNumber(long long int n)
+// {
+//     long long int ans = 0;
+
+//     while(n){
+//         ans = ans * 10 + (n % 10);
+//         n = n / 10;
+//     }
+
+//     return ans;
+// }
