db8d06b67e2eb8fd49f6a1f50ec30c57dbe00e8f
ANI1KET <aniketrouniyar12@gmail.com>
2024-02-29 14:23:32 +0530

    Reverse_of_a_number

---
+++ Reverse_of_a_number.cc
@@ -0,0 +1,18 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     int ans = 0;
+
+//     cin >> n;
+
+//     while (n)
+//     {
+//         ans = ans * 10 + (n % 10);
+//         n = n / 10;
+//     }
+
+//     cout << ans;
+// }
