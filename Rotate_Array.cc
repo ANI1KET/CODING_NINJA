f24d83868346a3d87d87d1144383bff984199fd1
ANI1KET <aniketrouniyar12@gmail.com>
2024-03-09 20:28:30 +0530

    Rotate_Array

---
+++ Rotate_Array.cc
@@ -0,0 +1,47 @@
+// vector<int> rotateArray(vector<int>arr, int k) {
+//     int n = arr.size();
+//     vector<int> a(n);
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         a[(i + (n - k)) % n] = arr[i];
+//     }
+
+//     return a;
+// }
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+void rotate(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        a[(i + (n - k)) % n] = nums[i];
+    }
+
+    nums = a;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+    }
+}
+
+int main()
+{
+    // vector<int> a = {1, 2, 3, 4, 5, 6, 7};
+    vector<int> a = {1, 2, 3, 4, 5};
+    int k = 3;
+
+    rotate(a, k);
+
+    return 0;
+}
